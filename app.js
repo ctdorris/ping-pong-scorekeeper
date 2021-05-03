ef568916a52675d7894e9163dada3f8ac8f4ab9f
ctdorris <ctdorris@gmail.com>
2021-05-02 20:56:37 -0500

    add span elements

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const maxScore = document.querySelector('#maxScore');
-const scoreboard = document.querySelector('#scoreboard');
+const p1Scoreboard = document.querySelector('#p1Score');
+const p2Scoreboard = document.querySelector('#p2Score')
 const player1 = document.querySelector('#player1');
 const player2 = document.querySelector('#player2');
 const reset = document.querySelector('#reset');
@@ -7,9 +8,9 @@ const reset = document.querySelector('#reset');
 let player1Score = 0;
 let player2Score = 0;
 
-function updateScore(score) {
+function updateScore(score, board) {
     const maxScoreValue = parseInt(maxScore.value);
-    scoreboard.innerText = `${player1Score} to ${player2Score}`;
+    board.innerText = score;
     if (maxScoreValue === score){
         player1.disabled = true;
         player2.disabled = true;
@@ -19,17 +20,18 @@ function updateScore(score) {
 reset.addEventListener('click', function(){
     player1Score = 0;
     player2Score = 0;
-    scoreboard.innerText = `${player1Score} to ${player2Score}`;
+    p1Scoreboard.innerText = player1Score;
+    p2Scoreboard.innerText = player2Score;
     player1.disabled = false;
     player2.disabled = false;
 });
 
 player1.addEventListener('click', function(){
     player1Score ++;
-    updateScore(player1Score);
+    updateScore(player1Score, p1Scoreboard);
 });
 
 player2.addEventListener('click', function(){
     player2Score ++;
-    updateScore(player2Score);
+    updateScore(player2Score, p2Scoreboard);
 });
