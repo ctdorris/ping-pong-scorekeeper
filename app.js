c9d1e8f2fd9f42e1bdbd7b4851c26d6b68daa088
ctdorris <ctdorris@gmail.com>
2021-05-05 08:45:57 -0500

    Added styling

--- app.js
+++ app.js
@@ -1,37 +1,109 @@
-const maxScore = document.querySelector('#maxScore');
-const p1Scoreboard = document.querySelector('#p1Score');
-const p2Scoreboard = document.querySelector('#p2Score')
-const player1 = document.querySelector('#player1');
-const player2 = document.querySelector('#player2');
-const reset = document.querySelector('#reset');
-
-let player1Score = 0;
-let player2Score = 0;
-
-function updateScore(score, board) {
-    const maxScoreValue = parseInt(maxScore.value);
-    board.innerText = score;
-    if (maxScoreValue === score){
-        player1.disabled = true;
-        player2.disabled = true;
-    }
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+};
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
 }
 
-reset.addEventListener('click', function(){
-    player1Score = 0;
-    player2Score = 0;
-    p1Scoreboard.innerText = player1Score;
-    p2Scoreboard.innerText = player2Score;
-    player1.disabled = false;
-    player2.disabled = false;
-});
 
-player1.addEventListener('click', function(){
-    player1Score ++;
-    updateScore(player1Score, p1Scoreboard);
-});
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
+let winningScore = 3;
+let isGameOver = false;
 
-player2.addEventListener('click', function(){
-    player2Score ++;
-    updateScore(player2Score, p2Scoreboard);
-});
+function updateScores(player, opponent){
+    if(!isGameOver){
+        player.score += 1;
+        if (player.score === winningScore){
+            isGameOver = true;
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        };
+        player.display.textContent = player.score;
+    };
+};
+
+p1.button.addEventListener('click', function(){
+    updateScores(p1, p2)
+})
+
+p2.button.addEventListener('click', function(){
+    updateScores(p2, p1)
+})
+
+winningScoreSelect.addEventListener('change', function(){
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset(){
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.innerText = 0;
+    p2Display.innerText = 0;
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
+}
+
+
+// Original code
+
+// const maxScore = document.querySelector('#maxScore');
+// const p1Scoreboard = document.querySelector('#p1Score');
+// const p2Scoreboard = document.querySelector('#p2Score')
+// const p1Button = document.querySelector('#p1Button');
+// const p2Button = document.querySelector('#player2');
+// const reset = document.querySelector('#reset');
+
+// let player1Score = 0;
+// let player2Score = 0;
+// let winningScore = 5;
+// let isGameOver = false;
+
+
+// function updateScore(score, board, player) {
+//     let maxScoreValue = parseInt(maxScore.value);
+//     // board.innerText = score;
+//     if (winningScore === score){
+//         // player1.disabled = true;
+//         // player2.disabled = true;
+//     } else if (player === 1){
+//         player1Score ++;
+//         board.textContent = player1Score;
+//     } else if (player === 2){
+//         player2Score++;
+//         board.textContent = player2Score;
+//     }
+// }
+
+// reset.addEventListener('click', function(){
+//     player1Score = 0;
+//     player2Score = 0;
+//     p1Scoreboard.innerText = player1Score;
+//     p2Scoreboard.innerText = player2Score;
+//     player1.disabled = false;
+//     player2.disabled = false;
+// });
+
+// player1.addEventListener('click', function(){
+//     // player1Score ++;
+//     let player = 1;
+//     updateScore(player1Score, p1Scoreboard, player);
+// });
+
+// player2.addEventListener('click', function(){
+//     // player2Score ++;
+//     let player = 2;
+//     updateScore(player2Score, p2Scoreboard, player);
+// });
